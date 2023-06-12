ffb92cd41a5be00a4ad61bc732a92ca0572b8842
Mayeu <m@mayeu.me>
2023-06-13 00:54:00 +0200

    feat(engine/util/module): add tests and refactor

--- snake-ecs/lib/engine/util/module.ex
+++ snake-ecs/lib/engine/util/module.ex
@@ -1,34 +1,39 @@
 defmodule Engine.Util.Module do
+  @spec get_entity_types() :: list(atom())
   def get_entity_types do
-    {:ok, modules} = :application.get_key(:elixir_rpg, :modules)
+    {:ok, modules} = :application.get_key(:snake, :modules)
 
     modules
     |> Enum.filter(fn module ->
       parts = Module.split(module)
-      # TODO: convert the moduleule name to string instead of using string (for compiling error)
-      match?(["EntityTypes" | _], parts)
+
+      # TODO: convert the module beginning name to string instead of using string (for compiling error)
+      # TODO: Also, consider making this a configuration. It should not be Engine.EntityTypes by default also, maybe just Entity.xxx, because it's not the entities of the engine.
+      match?(["Engine", "EntityTypes" | _], parts)
     end)
   end
 
+  @spec get_component_types() :: list(atom())
   def get_component_types do
-    {:ok, modules} = :application.get_key(:elixir_rpg, :modules)
+    {:ok, modules} = :application.get_key(:snake, :modules)
 
     modules
     |> Enum.filter(fn module ->
       parts = Module.split(module)
       # TODO: convert the moduleule name to string instead of using string (for compiling error)
-      match?(["ComponentTypes" | _], parts)
+      match?(["Engine", "ComponentTypes" | _], parts)
     end)
   end
 
+  @spec get_system_types() :: list(atom())
   def get_system_types do
-    {:ok, modules} = :application.get_key(:elixir_rpg, :modules)
+    {:ok, modules} = :application.get_key(:snake, :modules)
 
     modules
     |> Enum.filter(fn module ->
       parts = Module.split(module)
       # TODO: convert the moduleule name to string instead of using string (for compiling error)
-      match?(["RuntimeSystems" | _], parts)
+      match?(["Engine", "RuntimeSystems" | _], parts)
     end)
   end
 end
