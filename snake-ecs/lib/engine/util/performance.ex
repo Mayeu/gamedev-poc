b3bf12e6b186533fb1df7acba0edd404940a22ee
Mayeu <m@mayeu.me>
2023-06-13 12:55:56 +0200

    chore(engine/util/performance): add missing extension

--- snake-ecs/lib/engine/util/performance.ex
+++ snake-ecs/lib/engine/util/performance.ex
@@ -1,4 +1,5 @@
 defmodule Engine.Util.Performance do
+  # TODO: pretty sure we don't need that.
   def parallel_map(collection, func) do
     collection
     |> Enum.map(&Task.async(fn -> func.(&1) end))
